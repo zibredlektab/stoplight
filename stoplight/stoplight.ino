c6bb20ae0cef0f87b7e7072443e59bfcc31cafd2
zibredlektab <zibredlektab@gmail.com>
2025-03-11 14:54:14 -0400

    Code now properly calculates voltage from ADC

--- stoplight/stoplight.ino
+++ stoplight/stoplight.ino
@@ -1,4 +1,4 @@
-#define AIN A2
+#define BATT A2
 #define RED 1
 #define GREEN 0
 #define KNOB A3
@@ -8,27 +8,33 @@
 
 int count = 0; // count of ADC samples taken
 uint32_t battlvltemp = 0; // for averaging
-int battlvldivided = 0; // mean of ADC input
-int battlvl = 0; // mapped to voltage
-int prevbattlvl = 0; // voltage last loop
+long battlvlmean = 0; // mean of ADC input
+float battlvl = 0.0; // mapped to voltage
+float prevbattlvl = 0.0; // voltage last loop
 
 uint32_t lastblinktime = 0; // low voltage alert blink
 bool blinking = false;
 bool blinkon = false;
 
-int knoblvl = 0; // for dimming
-
 void setup() {
   // put your setup code here, to run once:
-  pinMode(AIN, INPUT); // battery voltage divider
+  pinMode(BATT, INPUT); // battery voltage divider
   pinMode(KNOB, INPUT); // dimmer
   pinMode(RED, OUTPUT); // red led
   pinMode(GREEN, OUTPUT); // green led
 }
 
 void setLED(int pin, int lvl) {
-  knoblvl = map(analogRead(KNOB), 0, 1023, 10, 1);
-  analogWrite(pin, 255 - (lvl/knoblvl));
+  long knoblvl = 0;
+  int knobcount = 0;
+  while (knobcount < 10) {
+    knoblvl += analogRead(KNOB);
+    knobcount++;
+  }
+  knobcount = 0;
+  knoblvl /= 10;
+  int dim = map(knoblvl, 0, 1023, 10, 1);
+  analogWrite(pin, 255 - (lvl/dim));
 }
 
 void loop() {
@@ -40,35 +46,42 @@ void loop() {
 
   count = 0;
   while (count++ < 100) {
-    battlvltemp += analogRead(AIN);
+    // take lots of readings to smooth everything out
+    battlvltemp += analogRead(BATT);
     delay(1);
   }
-  battlvltemp /= 100;
-  battlvldivided = battlvltemp;
-  battlvl = map(battlvldivided, 0, 1023, 0, 2015); // numbers below have been calibrated
+  battlvltemp /= 100; // take the mean
+  battlvlmean = battlvltemp;
+  battlvl = battlvlmean * (5.0 / 1023.0); // ADC reading to voltage
+  battlvl /= 0.248; // undo voltage divider (more precise resistors will help)
+  //battlvl = map(battlvlmean, 0, 1023, 0, 2015); // numbers below have been calibrated
+
 
   if ((battlvl > prevbattlvl) && (battlvl - prevbattlvl < HYSTERESIS)) {
     // voltage has increased, but by too small a value to matter
     // we do nothing
+    battlvl = prevbattlvl;
   } else {
     // voltage has either gone up, not changed, or gone down
     prevbattlvl = battlvl;
-    if (battlvl > 1300) { // 13v - 17v = green
-      setLED(RED, 0);
-      setLED(GREEN, 255);
-      blinking = false;
-    } else if (battlvl > 1200) { // 12v - 13v = yellow
-      setLED(RED, 200);
-      setLED(GREEN, 200);
-      blinking = false;
-    } else if (battlvl > 1170) { // 11.5 - 12v = red
-      setLED(RED, 255);
-      setLED(GREEN, 0);
-      blinking = false;
-    } else { // below 11.5 = flash
-      setLED(GREEN, 0);
-      blinking = true;
-    }
+  }
+
+  prevbattlvl = battlvl;
+  if (battlvl > 13.0) { // 13v - 17v = green
+    setLED(RED, 0);
+    setLED(GREEN, 255);
+    blinking = false;
+  } else if (battlvl > 12.4) { // 12.4v - 13v = yellow
+    setLED(RED, 100);
+    setLED(GREEN, 100);
+    blinking = false;
+  } else if (battlvl > 12.1) { // 12.1 - 12.4v = red
+    setLED(RED, 255);
+    setLED(GREEN, 0);
+    blinking = false;
+  } else { // below 12.1 = flash
+    setLED(GREEN, 0);
+    blinking = true;
   }
 
   if (blinking) {
