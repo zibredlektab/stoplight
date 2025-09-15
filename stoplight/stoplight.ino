7a7c5463fe1232a093e60d0e414f043b18cfef80
zibredlektab <zibredlektab@gmail.com>
2025-09-15 15:53:28 -0400

    Replaced dimmer with dim button. Currently functional

--- stoplight/stoplight.ino
+++ stoplight/stoplight.ino
@@ -1,9 +1,16 @@
-#define BATT A2
+#include <EEPROM.h>
+
+#define BATT A1
 #define RED 1
 #define GREEN 0
-#define KNOB A3
+#define BUTTON 3
 
-#define HYSTERESIS 10
+#define HYSTERESIS 0.5
+#define DEBOUNCE 100
+
+#define GREENTOYELLOW 13.0
+#define YELLOWTORED 12.5
+#define REDTOFLASH 11.9
 
 
 int count = 0; // count of ADC samples taken
@@ -16,25 +23,53 @@ uint32_t lastblinktime = 0; // low voltage alert blink
 bool blinking = false;
 bool blinkon = false;
 
+int dimlvl; // 0-2, 2 being brightest
+uint32_t lastbuttontime = 0; // button debounce
+bool buttonreleased = false;
+
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(BATT, INPUT); // battery voltage divider
-  pinMode(KNOB, INPUT); // dimmer
+  pinMode(BUTTON, INPUT_PULLUP); // dimmer
   pinMode(RED, OUTPUT); // red led
   pinMode(GREEN, OUTPUT); // green led
+
+  EEPROM.get(1, dimlvl);
+
+/*
+  delay(1000);
+
+  setLED(RED, 255);
+  setLED(GREEN, 0);
+  delay(500);
+  setLED(RED, 0);
+  setLED(GREEN, 0);
+  delay(100);
+  setLED(RED, 210);
+  setLED(GREEN, 80);
+  delay(500);
+  setLED(RED, 0);
+  setLED(GREEN, 0);
+  delay(100);
+  setLED(RED, 0);
+  setLED(GREEN, 255);
+  delay(500);
+  setLED(RED, 0);
+  setLED(GREEN, 0);
+  delay(1000);
+  */
+  
 }
 
 void setLED(int pin, int lvl) {
-  long knoblvl = 0;
-  int knobcount = 0;
-  while (knobcount < 10) {
-    knoblvl += analogRead(KNOB);
-    knobcount++;
-  }
-  knobcount = 0;
-  knoblvl /= 10;
-  int dim = map(knoblvl, 0, 1023, 10, 1);
-  analogWrite(pin, 255 - (lvl/dim));
+
+  int dim = 4;
+  if (dimlvl == 0) dim = 4;
+  if (dimlvl == 1) dim = 2;
+  if (dimlvl == 2) dim = 1;
+
+  analogWrite(pin, lvl/dim);
 }
 
 void loop() {
@@ -54,6 +89,7 @@ void loop() {
   battlvlmean = battlvltemp;
   battlvl = battlvlmean * (5.0 / 1023.0); // ADC reading to voltage
   battlvl /= 0.248; // undo voltage divider (more precise resistors will help)
+  battlvl -= 0.2; // correct for offset
   //battlvl = map(battlvlmean, 0, 1023, 0, 2015); // numbers below have been calibrated
 
 
@@ -67,15 +103,15 @@ void loop() {
   }
 
   prevbattlvl = battlvl;
-  if (battlvl > 13.0) { // 13v - 17v = green
+  if (battlvl > GREENTOYELLOW) { // 13v - 17v = green
     setLED(RED, 0);
     setLED(GREEN, 255);
     blinking = false;
-  } else if (battlvl > 12.4) { // 12.4v - 13v = yellow
-    setLED(RED, 100);
-    setLED(GREEN, 100);
+  } else if (battlvl > YELLOWTORED) { // 12.4v - 13v = yellow
+    setLED(RED, 210);
+    setLED(GREEN, 50);
     blinking = false;
-  } else if (battlvl > 12.1) { // 12.1 - 12.4v = red
+  } else if (battlvl > REDTOFLASH) { // 12.1 - 12.4v = red
     setLED(RED, 255);
     setLED(GREEN, 0);
     blinking = false;
@@ -87,7 +123,7 @@ void loop() {
   if (blinking) {
     if (millis() >= lastblinktime + 1000) {
       if (blinkon) {
-        setLED(RED, 100);
+        setLED(RED, 60);
         blinkon = false;
       } else {
         setLED(RED, 255);
@@ -95,5 +131,17 @@ void loop() {
       }
       lastblinktime = millis();
     }
+  }
+
+  if (digitalRead(BUTTON)) { // button is up
+    if (buttonreleased && millis() >= lastbuttontime + DEBOUNCE) { // button was not previously up (just released)
+      buttonreleased = false;
+      dimlvl++;
+      if (dimlvl > 2) dimlvl = 0;
+      EEPROM.put(1, dimlvl);
+      lastbuttontime = millis();
+    }
+  } else {
+    buttonreleased = true;
   }
 }
